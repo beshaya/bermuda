de608c54a0a3e54301e28cc83a56dc495b2a4e1a
Elena Churilov <elena.churilov@gmail.com>
2020-06-27 20:42:12 -0700

    Create Router component

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
+import App from './App';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  );
+}
+
+export default Router;
