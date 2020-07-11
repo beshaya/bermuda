4a3487d1203bf9b5eddf0a5069c258d95132f77c
Benjamin Shaya <bshaya@alum.mit.edu>
2020-07-11 07:52:50 -0600

    Add login with google via firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { Redirect } from 'react-router-dom'
 import './App.css';
+import {SignOut } from './firebase';
+import { UserContext } from "./providers/userProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Bermuda</h1>
-      </header>
-    </div>
+    const user = useContext(UserContext);
+    if (!user) {
+      return <Redirect to='/login' />
+    }
+    console.log(user);
+    return (
+      <div className="App">
+        <button onClick={SignOut}> Log Out </button>
+        <header className="App-header">
+          <h1>Bermuda</h1>
+          Welcome {user.displayName}
+        </header>
+     </div>
   );
 }
 
