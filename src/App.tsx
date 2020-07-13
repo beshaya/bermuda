3904b28c8ab138437ce6a75e18b774f32ca4691f
Elena Churilov <elena.churilov@gmail.com>
2020-07-13 00:08:21 -0700

    Add dummy elements

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Map from './components/Map';
 import {SignOut } from './firebase';
 import { UserContext } from "./providers/userProvider";
+import schooner from "./assets/ships/two-masted-schooner.jpg"
 
 function App() {
   const user = useContext(UserContext);
@@ -19,6 +20,19 @@ function App() {
       </header>
       <div className="content">
         <Map />
+        <div className="sidebar">
+          <div className="turn-info">
+            <p>Turn Number: 1</p>
+            <p>Waiting for GM...</p>
+          </div>
+          <div className="tile-info">
+            <h2>Tile Info</h2>
+            <p>Bilged on her anchor league gunwalls pink flogging measured fer yer chains hang the jib black jack bowsprit doubloon. Salmagundi wench pirate reef sails brig furl stern hang the jib broadside spike. Cackle fruit quarterdeck schooner case shot swing the lead measured fer yer chains Pieces of Eight draught red ensign pinnace. </p>
+          </div>
+          <div className="tile-img">
+            <img src={schooner} alt="two masted schooner" />
+          </div>
+        </div>
       </div>
     </div>
   );
